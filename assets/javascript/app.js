65df0a13c0a28dae276496604a8a796c698493a2
ElyriaG <ElyriaG@users.noreply.github.com>
2020-09-16 13:20:53 -0500

    youtube testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,148 +36,205 @@ $(".dropdown-menu a").on("click", function () {
   //Loads all functions
   loader();
 
- 
-// --------------------------------- Function Loader to Call onClick jQuery Trigger ---------------------------
-function loader() {
-  spotify();
-  gif();
-  quotes();
-  youTube();
-}
-/* ----------------------------------------- GIF Function ---------------------------------- */
-function gif() {
-  var queryURL =
-    "https://api.giphy.com/v1/gifs/search?q=" +
-    topic +
-    "&api_key=FHh9mWC90FyTtVYHXSy5uFhHubUvyLWb&limit=20";
+  // --------------------------------- Function Loader to Call onClick jQuery Trigger ---------------------------
+  function loader() {
+    spotify();
+    gif();
+    quotes();
+    Youtube();
+  }
+  /* ----------------------------------------- GIF Function ---------------------------------- */
+  function gif() {
+    var queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      topic +
+      "&api_key=FHh9mWC90FyTtVYHXSy5uFhHubUvyLWb&limit=20";
 
-  // Ajax call to get random gif 
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    var random = Math.floor(Math.random() * 21);
+    // Ajax call to get random gif
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      var random = Math.floor(Math.random() * 21);
 
-    var results = response.data[random];
-    var topicGif = $("<img>").addClass("gif");
-    topicGif.attr("src", results.images.fixed_height.url);
+      var results = response.data[random];
+      var topicGif = $("<img>").addClass("gif");
+      topicGif.attr("src", results.images.fixed_height.url);
 
-    $(".gif").append(topicGif);
-  });
+      $(".gif").append(topicGif);
+    });
 
-  /* ----------------------------------------- Quotes Function ---------------------------------- */
-}
-function quotes() {
-  var queryURL = "https://quotable-quotes.p.rapidapi.com/randomQuotes";
+    /* ----------------------------------------- Quotes Function ---------------------------------- */
+  }
+  function quotes() {
+    var queryURL = "https://quotable-quotes.p.rapidapi.com/randomQuotes";
 
-  // Ajax call to get quote
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Host": "quotable-quotes.p.rapidapi.com",
-      "X-RapidAPI-Key": "6f84e5eceamsh5c5d979bf355c11p1f651ajsneb75d52cb90c",
-    },
-  }).then(function (response) {
-    var quote = $("<h1>").text(response.quote);
-    var author = $("<h3>").text("– " + response.author);
-
-    $(".quoteText").prepend(quote);
-    $(".quoteAuthor").prepend(author);
-  });
-}
-
-// --------------------------------- Spotify Function ---------------------------
-function spotify() {
-
-  //Variable to capture the bearer token received on authentication API call (POST)
-  var bearer = "";
-
-  //Token type required to authorized on the 2nd API call (GET)
-  var tokenType = "";
-
-  // Spotify API URL for for authentication with cors anywhere to address CORS errors on authentication
-  var authURL =
-    "https://accounts.spotify.com/api/token";
-
-  //Spotify API URL to query for Playist
-  var queryURL =
-    "https://api.spotify.com/v1/search?q=" + topic + "&type=playlist";
-
-  //API POST fora authorization
-  $.ajax({
-    url: authURL,
-    method: "POST",
-    headers: {
-      Authorization:
-        "Basic ZmFjYzg2MTQxZGNmNDAyNWFmYWE1MGRmZTE4Zjg1YjE6YTJmMWM4ODhmOTUxNDFjNTliNzYwMzk5ZGY0NWIyYzI=",
-    },
-    data: {
-      grant_type: "client_credentials",
-    },
-  }).then(function (response) {
-    bearer = response.access_token;
-    tokenType = response.token_type;
-
-    //Concatenate the token type and bearer token 
-    var auth = tokenType + " " + bearer;
-
-    //Ajax call for the playlist 
+    // Ajax call to get quote
     $.ajax({
       url: queryURL,
       method: "GET",
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: auth,
+        "X-RapidAPI-Host": "quotable-quotes.p.rapidapi.com",
+        "X-RapidAPI-Key": "6f84e5eceamsh5c5d979bf355c11p1f651ajsneb75d52cb90c",
       },
     }).then(function (response) {
-      //Randomly choose a playlist from the API response
-      i = Math.floor(Math.random() * response.playlists.items.length);
+      var quote = $("<h1>").text(response.quote);
+      var author = $("<h3>").text("– " + response.author);
 
-      //Get response and build url to pass into spotify embed player
-      var id = response.playlists.items[i].id;
-      var urlBuilder = "https://open.spotify.com/embed/playlist/";
-      var link = urlBuilder + id;
-
-      //jQuery to update iframe src and load embedded player into DOM
-      $("#player").attr("src", link);
+      $(".quoteText").prepend(quote);
+      $(".quoteAuthor").prepend(author);
     });
-  });
-}
-
-// --------------------------------- Youtube Function ---------------------------
-function youTube() {
-  gapi.client.setApiKey("AIzaSyAxPlN1cE06Y33mEtSaUUPR8Yv5LtGWHks");
-  return gapi.client
-    .load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-    .then(function () {
-      execute();
-    });
-
-  // Make sure the client is loaded before calling this method.
-  function execute() {
-    return gapi.client.youtube.search
-      .list({
-        part: "snippet",
-        maxResults: 10,
-        q: "yoga" + topic,
-        topicId: "yoga",
-      })
-      .then(function (response) {
-        // Handle the results here (response.result has the parsed body).
-        console.log("Response", response);
-        console.log("videoId " + response.result.items[0].id.videoId);
-        var urlBuild = "https://www.youtube.com/embed/";
-        i = Math.floor(Math.random() * response.result.items.length);
-        // var tag = document.createElement('script');
-        var urlBuild = "https://www.youtube.com/embed/";
-        var videoId = response.result.items[i].id.videoId;
-        var ytUrl = urlBuild + videoId;
-        var yt = $(".player").attr("src", ytUrl);
-        console.log("yt", yt);
-      });
   }
-}
-// --------------------------------- Required for Youtube GAPI ---------------------------
-gapi.load("client");
+
+  // --------------------------------- Spotify Function ---------------------------
+  function spotify() {
+    //Variable to capture the bearer token received on authentication API call (POST)
+    var bearer = "";
+
+    //Token type required to authorized on the 2nd API call (GET)
+    var tokenType = "";
+
+    // Spotify API URL for for authentication with cors anywhere to address CORS errors on authentication
+    var authURL = "https://accounts.spotify.com/api/token";
+
+    //Spotify API URL to query for Playist
+    var queryURL =
+      "https://api.spotify.com/v1/search?q=" + topic + "&type=playlist";
+
+    //API POST fora authorization
+    $.ajax({
+      url: authURL,
+      method: "POST",
+      headers: {
+        Authorization:
+          "Basic ZmFjYzg2MTQxZGNmNDAyNWFmYWE1MGRmZTE4Zjg1YjE6YTJmMWM4ODhmOTUxNDFjNTliNzYwMzk5ZGY0NWIyYzI=",
+      },
+      data: {
+        grant_type: "client_credentials",
+      },
+    }).then(function (response) {
+      bearer = response.access_token;
+      tokenType = response.token_type;
+
+      //Concatenate the token type and bearer token
+      var auth = tokenType + " " + bearer;
+
+      //Ajax call for the playlist
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: auth,
+        },
+      }).then(function (response) {
+        //Randomly choose a playlist from the API response
+        i = Math.floor(Math.random() * response.playlists.items.length);
+
+        //Get response and build url to pass into spotify embed player
+        var id = response.playlists.items[i].id;
+        var urlBuilder = "https://open.spotify.com/embed/playlist/";
+        var link = urlBuilder + id;
+
+        //jQuery to update iframe src and load embedded player into DOM
+        $("#player").attr("src", link);
+      });
+    });
+  }
+
+  // --------------------------------- Youtube Function ---------------------------
+  function Youtube() {
+    function authenticate() {
+      return gapi.auth2
+        .getAuthInstance()
+        .signIn({ scope: "https://www.googleapis.com/auth/youtube.force-ssl" })
+        .then(
+          function () {
+            console.log("Sign-in successful");
+          },
+          function (err) {
+            console.error("Error signing in", err);
+          }
+        );
+    }
+    function loadClient() {
+      gapi.client.setApiKey("AIzaSyCW6rZcnHcQI27E5DHDQQliahVnV3afeE8");
+      return gapi.client
+        .load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(
+          function () {
+            console.log("GAPI client loaded for API");
+          },
+          function (err) {
+            console.error("Error loading GAPI client for API", err);
+          }
+        );
+    }
+    // Make sure the client is loaded and sign-in is complete before calling this method.
+    function execute() {
+      return gapi.client.youtube.search
+        .list({
+          part: ["snippet"],
+          maxResults: 10,
+          q: "angry yoga",
+        })
+        .then(
+          function (response) {
+            // Handle the results here (response.result has the parsed body).
+            console.log("Response", response);
+            console.log("Video Info: " + response.result.items[0].id.videoId);
+
+            i = Math.floor(Math.random() * response.result.items.length);
+            var urlBuild = "https://www.youtube.com/embed/";
+            var videoId = response.result.items[i].id.videoId;
+            var ytUrl = urlBuild + videoId;
+            var yt = $(".player").attr("src", ytUrl);
+          },
+          function (err) {
+            console.error("Execute error", err);
+          }
+        );
+    }
+    gapi.load("client:auth2", function () {
+      gapi.auth2.init({
+        client_id:
+          "861134492961-a4df07k1cog92g7pevh42okaj38d57cc.apps.googleusercontent.com",
+      });
+    });
+  }
+});
+// function youTube() {
+//   gapi.client.setApiKey("AIzaSyAxPlN1cE06Y33mEtSaUUPR8Yv5LtGWHks");
+//   return gapi.client
+//     .load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+//     .then(function () {
+//       execute();
+//     });
+
+//   // Make sure the client is loaded before calling this method.
+//   function execute() {
+//     return gapi.client.youtube.search
+//       .list({
+//         part: "snippet",
+//         maxResults: 10,
+//         q: "yoga" + topic,
+//         topicId: "yoga",
+//       })
+//       .then(function (response) {
+//         // Handle the results here (response.result has the parsed body).
+//         console.log("Response", response);
+//         console.log("videoId " + response.result.items[0].id.videoId);
+//         var urlBuild = "https://www.youtube.com/embed/";
+//         i = Math.floor(Math.random() * response.result.items.length);
+//         // var tag = document.createElement('script');
+//         var urlBuild = "https://www.youtube.com/embed/";
+//         var videoId = response.result.items[i].id.videoId;
+//         var ytUrl = urlBuild + videoId;
+//         var yt = $(".player").attr("src", ytUrl);
+//         console.log("yt", yt);
+//       });
+//   }
+// }
+// // --------------------------------- Required for Youtube GAPI ---------------------------
+// gapi.load("client")
